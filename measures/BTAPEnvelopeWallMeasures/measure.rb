bb5f7ada809269c46e05a4ca277c5f811456fe1e
phylroy <phylroy.lopez@gmail.com>
2018-04-06 20:40:27 +0000

    stuff

--- measures/BTAPEnvelopeWallMeasures/measure.rb
+++ measures/BTAPEnvelopeWallMeasures/measure.rb
@@ -22,56 +22,58 @@ class BTAPExteriorWallMeasure < OpenStudio::Measure::ModelMeasure
   # define the arguments that the user will input
   def arguments(model)
     args = OpenStudio::Ruleset::OSArgumentVector.new
+    baseline = 'baseline'
 
     # Conductance value entered
+
     ecm_exterior_wall_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_wall_conductance", true)
     ecm_exterior_wall_conductance.setDisplayName('Exterior Wall Conductance (W/m2 K)')
-    ecm_exterior_wall_conductance.setDefaultValue('baseline')
+    ecm_exterior_wall_conductance.setDefaultValue(baseline)
     args << ecm_exterior_wall_conductance
 
     ecm_exterior_roof_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_roof_conductance", true)
     ecm_exterior_roof_conductance.setDisplayName('Exterior Roof Conductance (W/m2 K)')
-    ecm_exterior_roof_conductance.setDefaultValue('baseline')
+    ecm_exterior_roof_conductance.setDefaultValue(baseline)
     args << ecm_exterior_roof_conductance
 
     ecm_exterior_floor_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_floor_conductance", true)
     ecm_exterior_floor_conductance.setDisplayName('Exterior Floor Conductance (W/m2 K)')
-    ecm_exterior_floor_conductance.setDefaultValue('baseline')
+    ecm_exterior_floor_conductance.setDefaultValue(baseline)
     args << ecm_exterior_floor_conductance
 
     ecm_ground_wall_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_ground_wall_conductance", true)
     ecm_ground_wall_conductance.setDisplayName('Exterior Ground Wall Conductance (W/m2 K)')
-    ecm_ground_wall_conductance.setDefaultValue('baseline')
+    ecm_ground_wall_conductance.setDefaultValue(baseline)
     args << ecm_ground_wall_conductance
 
     ecm_ground_roof_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_ground_roof_conductance", true)
     ecm_ground_roof_conductance.setDisplayName('Ground Roof Conductance (W/m2 K)')
-    ecm_ground_roof_conductance.setDefaultValue('baseline')
+    ecm_ground_roof_conductance.setDefaultValue(baseline)
     args << ecm_ground_roof_conductance
 
     ecm_ground_floor_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_ground_floor_conductance", true)
     ecm_ground_floor_conductance.setDisplayName('Ground Floor Conductance (W/m2 K)')
-    ecm_ground_floor_conductance.setDefaultValue('baseline')
+    ecm_ground_floor_conductance.setDefaultValue(baseline)
     args << ecm_ground_floor_conductance
 
     ecm_exterior_window_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_window_conductance", true)
     ecm_exterior_window_conductance.setDisplayName('Window Conductance (W/m2 K)')
-    ecm_exterior_window_conductance.setDefaultValue('baseline')
+    ecm_exterior_window_conductance.setDefaultValue(baseline)
     args << ecm_exterior_window_conductance
 
     ecm_exterior_skylight_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_skylight_conductance", true)
     ecm_exterior_skylight_conductance.setDisplayName('Skylight Conductance (W/m2 K)')
-    ecm_exterior_skylight_conductance.setDefaultValue('baseline')
+    ecm_exterior_skylight_conductance.setDefaultValue(baseline)
     args << ecm_exterior_skylight_conductance
 
     ecm_exterior_door_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_door_conductance", true)
     ecm_exterior_door_conductance.setDisplayName('Door Conductance (W/m2 K)')
-    ecm_exterior_door_conductance.setDefaultValue('baseline')
+    ecm_exterior_door_conductance.setDefaultValue(baseline)
     args << ecm_exterior_door_conductance
 
     ecm_exterior_overhead_door_conductance = OpenStudio::Ruleset::OSArgument.makeStringArgument("ecm_exterior_overhead_door_conductance", true)
     ecm_exterior_overhead_door_conductance.setDisplayName('Overhead Door Conductance (W/m2 K)')
-    ecm_exterior_overhead_door_conductance.setDefaultValue('baseline')
+    ecm_exterior_overhead_door_conductance.setDefaultValue(baseline)
     args << ecm_exterior_overhead_door_conductance
 
     return args
@@ -86,51 +88,137 @@ class BTAPExteriorWallMeasure < OpenStudio::Measure::ModelMeasure
       return false
     end
 
+    #Set default rsi values to nil.
+    wall_rsi = nil
+    floor_rsi = nil
+    roof_rsi = nil
+    ground_wall_rsi = nil
+    ground_floor_rsi = nil
+    ground_roof_rsi = nil
+    door_rsi = nil
+    window_rsi = nil
 
+
+    #
     ecm_exterior_wall_conductance = runner.getStringArgumentValue('ecm_exterior_wall_conductance', user_arguments)
-    return true if ecm_exterior_wall_conductance == "baseline"
-
-    # assign the user inputs to variables
-    ecm_exterior_wall_conductance = ecm_exterior_wall_conductance.to_f
-
-    #check if conductance is negative or 0
-    if ecm_exterior_wall_conductance <= 0
-      runner.registerError("Conductance is less than or equal to 0")
+    if not wall_rsi.nil? and wall_rsi <= 0
+      runner.registerError("ecm_exterior_wall_conductance is less than or equal to 0")
       return false
     end
 
-    #Get surfaces from model
-    model.getDefaultConstructionSets.each do |default_surface_construction_set|
 
-      # convert conductance values to rsi values. (Note: we should really be only using conductances)
-      wall_rsi = 1.0 / ecm_exterior_wall_conductance
-      floor_rsi = nil
-      roof_rsi = nil
-      ground_wall_rsi = nil
-      ground_floor_rsi = nil
-      ground_roof_rsi = nil
-      door_rsi = nil
-      window_rsi = nil
-      BTAP::Resources::Envelope::ConstructionSets.customize_default_surface_construction_set_rsi!(model,
-                                                                                                  new_name,
-                                                                                                  default_surface_construction_set,
-                                                                                                  wall_rsi, floor_rsi, roof_rsi,
-                                                                                                  ground_wall_rsi, ground_floor_rsi, ground_roof_rsi,
-                                                                                                  window_rsi, nil, nil,
-                                                                                                  window_rsi, nil, nil,
-                                                                                                  door_rsi,
-                                                                                                  door_rsi, nil, nil,
-                                                                                                  door_rsi,
-                                                                                                  window_rsi, nil, nil,
-                                                                                                  window_rsi, nil, nil,
-                                                                                                  window_rsi, nil, nil)
+    outdoor_surfaces = BTAP::Geometry::Surfaces::filter_by_boundary_condition(model.getSurfaces(), "Outdoors")
+    outdoor_subsurfaces = BTAP::Geometry::Surfaces::get_subsurfaces_from_surfaces(outdoor_surfaces)
+    ground_surfaces = BTAP::Geometry::Surfaces::filter_by_boundary_condition(model.getSurfaces(), "Ground")
 
+    ext_wall_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(outdoor_surfaces, "Wall")
+    ext_roof_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(outdoor_surfaces, "RoofCeiling")
+    ext_floor_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(outdoor_surfaces, "Floor")
+    gnd_wall_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(ground_surfaces, "Wall")
+    gnd_roof_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(ground_surfaces, "RoofCeiling")
+    gnd_floor_surfaces = BTAP::Geometry::Surfaces::filter_by_surface_types(ground_surfaces, "Floor")
+    ext_windows = BTAP::Geometry::Surfaces::filter_subsurfaces_by_types(outdoor_subsurfaces, ["FixedWindow", "OperableWindow"])
+    ext_skylights = BTAP::Geometry::Surfaces::filter_subsurfaces_by_types(outdoor_subsurfaces, ["Skylight", "TubularDaylightDiffuser", "TubularDaylightDome"])
+    ext_doors = BTAP::Geometry::Surfaces::filter_subsurfaces_by_types(outdoor_subsurfaces, ["Door", "GlassDoor"])
+    ext_overhead_doors = BTAP::Geometry::Surfaces::filter_subsurfaces_by_types(outdoor_subsurfaces, ["OverheadDoor"])
+
+    unless ecm_exterior_wall_conductance == 'baseline'
+      appy_conductances_to_ext_surfaces(model, ext_wall_surfaces, ecm_exterior_wall_conductance)
     end
 
     return true
-
   end
 
+  def appy_conductances_to_ext_surfaces(model, surfaces, conductance)
+    surfaces.each do |surface|
+      construction = OpenStudio::Model::getConstructionByName(surface.model, surface.construction.get.name.to_s).get
+      new_construction_name = "#{surface.construction.get.name.to_s} Cond=#{conductance}"
+      new_construction = OpenStudio::Model::getConstructionByName(surface.model, new_construction_name)
+      target_u_value_ip = OpenStudio.convert(conductance.to_f, 'W/m^2*K', 'Btu/ft^2*hr*R').get
+      if new_construction.empty?
+        #create new construction.
+        #create a copy
+        new_construction = self.deep_copy(model, construction)
+        standard = Standard.new()
+
+        BTAP::Resources::Envelope::Constructions::find_and_set_insulaton_layer(model, [new_construction])
+        standard.construction_set_u_value(new_construction,
+                                          target_u_value_ip.to_f,
+                                          find_and_set_insulaton_layer(model, new_construction).name.get,
+                                          intended_surface_type = nil,
+                                          false,
+                                          false
+        )
+        new_construction.setName(new_construction_name)
+      else
+        new_construction = new_construction.get
+      end
+      surface.setConstruction(new_construction)
+    end
+  end
+
+  #This will create a deep copy of the construction
+  #@author Phylroy A. Lopez <plopez@nrcan.gc.ca>
+  #@param model [OpenStudio::Model::Model]
+  #@param construction <String>
+  #@return [String] new_construction
+  def deep_copy(model, construction)
+    construction = BTAP::Common::validate_array(model, construction, "Construction").first
+    new_construction = construction.clone.to_Construction.get
+    #interating through layers."
+    (0..new_construction.layers.length-1).each do |layernumber|
+      #cloning material"
+      cloned_layer = new_construction.getLayer(layernumber).clone.to_Material.get
+      #"setting material to new construction."
+      new_construction.setLayer(layernumber, cloned_layer)
+    end
+    return new_construction
+  end
+
+  #This method will search through the layers and find the layer with the
+  #lowest conductance and set that as the insulation layer. Note: Concrete walls
+  #or slabs with no insulation layer but with a carper will see the carpet as the
+  #insulation layer.
+  #@author Phylroy A. Lopez <plopez@nrcan.gc.ca>
+  #@param model [OpenStudio::Model::Model]
+  #@param constructions_array [BTAP::Common::validate_array]
+  #@return <String> insulating_layers
+  def find_and_set_insulaton_layer(model, construction)
+
+    insulating_layers = Array.new()
+    return_material = ""
+    #skip if already has an insulation layer set.
+    if construction.insulation.empty?
+      #find insulation layer
+      min_conductance = 100.0
+      #loop through Layers
+      construction.layers.each do |layer|
+        #try casting the layer to an OpaqueMaterial.
+        material = nil
+        material = layer.to_OpaqueMaterial.get unless layer.to_OpaqueMaterial.empty?
+        material = layer.to_FenestrationMaterial.get unless layer.to_FenestrationMaterial.empty?
+        #check if the cast was successful, then find the insulation layer.
+        unless nil == material
+
+          if BTAP::Resources::Envelope::Materials::get_conductance(material) < min_conductance
+            #Keep track of the highest thermal resistance value.
+            min_conductance = BTAP::Resources::Envelope::Materials::get_conductance(material)
+            return_material = material
+            unless material.to_OpaqueMaterial.empty?
+              construction.setInsulation(material)
+            end
+          end
+        end
+      end
+      if construction.insulation.empty? and construction.isOpaque
+        raise ("construction #{construction.name.get.to_s} insulation layer could not be set!. This occurs when a insulation layer is duplicated in the construction.")
+      end
+    else
+      return_material = construction.insulation.get
+    end
+
+    return return_material
+  end
 end
 
 # register the measure to be used by the application
