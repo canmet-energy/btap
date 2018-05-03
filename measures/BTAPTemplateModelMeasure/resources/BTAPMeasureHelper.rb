2c6b7df767ec586aec57dc64a14562b08ea3430a
phylroy <phylroy.lopez@gmail.com>
2018-05-03 14:03:06 +0000

    added tests to run json_input mode and string_double mode.

--- measures/BTAPTemplateModelMeasure/resources/BTAPMeasureHelper.rb
+++ measures/BTAPTemplateModelMeasure/resources/BTAPMeasureHelper.rb
@@ -11,10 +11,10 @@ module BTAPMeasureHelper
       @measure_interface_detailed.each do |argument|
         json_default[argument['name']] = argument["default_value"]
       end
-      default = JSON.pretty_generate( json_default )
+      default = JSON.pretty_generate(json_default)
       arg = OpenStudio::Ruleset::OSArgument.makeStringArgument('json_input', true)
       arg.setDisplayName('Contains a json version of the input as a single package.')
-      arg.setDefaultValue( default )
+      arg.setDefaultValue(default)
       args << arg
     else
       # Conductances for all surfaces and subsurfaces.
@@ -132,6 +132,7 @@ module BTAPMeasureHelper
     end
     return values
   end
+
   # Helper method to see if str is a valid float.
   def valid_float?(str)
     !!Float(str) rescue false
@@ -144,85 +145,98 @@ module BTAPMeasureTestHelper
 
   #Boiler plate to default values and number of arguments against what is in your test's setup method.
   def test_arguments_and_defaults
-    # Create an instance of the measure
-    measure = get_measure_object()
-    model = OpenStudio::Model::Model.new
+    [true, false].each do |json_input|
+      [true, false].each do |string_double|
+        @use_json_package = json_input
+        @use_string_double = string_double
 
-    # Create an instance of a runner
-    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+        # Create an instance of the measure
+        measure = get_measure_object()
+        measure.use_json_package = @use_json_package
+        measure.use_string_double = @use_string_double
+        model = OpenStudio::Model::Model.new
 
-    # Test arguments and defaults
-    arguments = measure.arguments(model)
-    #convert whatever the input was into a hash. Then test.
+        # Create an instance of a runner
+        runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
-    #check number of arguments.
-    if @use_json_package
-      assert_equal(@measure_interface_detailed.size, JSON.parse(arguments[0].defaultValueAsString).size, "The measure should have #{@measure_interface_detailed.size} but actually has #{arguments.size}. Here the the arguement expected #{@measure_interface_detailed} and this is the actual #{arguments}")
-    else
-      assert_equal(@measure_interface_detailed.size, arguments.size, "The measure should have #{@measure_interface_detailed.size} but actually has #{arguments.size}. Here the the arguement expected #{@measure_interface_detailed} and this is the actual #{arguments}")
-      (@measure_interface_detailed).each_with_index do |argument_expected, index|
-        assert_equal(argument_expected['name'], arguments[index].name, "Measure argument name of #{argument_expected['name']} was expected, but got #{arguments[index].name} instead.")
-        assert_equal(argument_expected['display_name'], arguments[index].displayName, "Display name for argument #{argument_expected['name']} was expected to be #{argument_expected['display_name']}, but got #{arguments[index].displayName} instead.")
-        case argument_type(arguments[index])
-          when "String", "Choice"
-            assert_equal(argument_expected['default_value'].to_s, arguments[index].defaultValueAsString, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
-          when "Double", "Integer"
-            assert_equal(argument_expected['default_value'].to_f, arguments[index].defaultValueAsDouble.to_f, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
-          when "Bool"
-            assert_equal(argument_expected['default_value'], arguments[index].defaultValueAsBool, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
+        # Test arguments and defaults
+        arguments = measure.arguments(model)
+        #convert whatever the input was into a hash. Then test.
+
+        #check number of arguments.
+        if @use_json_package
+          assert_equal(@measure_interface_detailed.size, JSON.parse(arguments[0].defaultValueAsString).size, "The measure should have #{@measure_interface_detailed.size} but actually has #{arguments.size}. Here the the arguement expected #{@measure_interface_detailed} and this is the actual #{arguments}")
+        else
+          assert_equal(@measure_interface_detailed.size, arguments.size, "The measure should have #{@measure_interface_detailed.size} but actually has #{arguments.size}. Here the the arguement expected #{@measure_interface_detailed} and this is the actual #{arguments}")
+          (@measure_interface_detailed).each_with_index do |argument_expected, index|
+            assert_equal(argument_expected['name'], arguments[index].name, "Measure argument name of #{argument_expected['name']} was expected, but got #{arguments[index].name} instead.")
+            assert_equal(argument_expected['display_name'], arguments[index].displayName, "Display name for argument #{argument_expected['name']} was expected to be #{argument_expected['display_name']}, but got #{arguments[index].displayName} instead.")
+            case argument_type(arguments[index])
+              when "String", "Choice"
+                assert_equal(argument_expected['default_value'].to_s, arguments[index].defaultValueAsString, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
+              when "Double", "Integer"
+                assert_equal(argument_expected['default_value'].to_f, arguments[index].defaultValueAsDouble.to_f, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
+              when "Bool"
+                assert_equal(argument_expected['default_value'], arguments[index].defaultValueAsBool, "The default value for argument #{argument_expected['name']} was #{argument_expected['default_value']}, but actual was #{arguments[index].defaultValueAsString}")
+            end
+          end
         end
       end
     end
-
   end
+
   # Test argument ranges.
   def test_argument_ranges
+    [true, false].each do |json_input|
+      [true, false].each do |string_double|
+        @use_json_package = json_input
+        @use_string_double = string_double
+        (@measure_interface_detailed).each_with_index do |argument|
+          if argument['type'] == 'Double' or argument['type'] == 'StringDouble'
+            puts "testing range for #{argument['name']} "
+            #Check over max
+            if not argument['max_double_value'].nil?
+              puts "testing max limit"
+              model = OpenStudio::Model::Model.new
+              input_arguments = @good_input_arguments.clone
+              over_max_value = argument['max_double_value'].to_f + 1.0
+              over_max_value = over_max_value.to_s if argument['type'].downcase == "StringDouble".downcase
+              input_arguments[argument['name']] = over_max_value
+              puts "Testing argument #{argument['name']} max limit of #{argument['max_double_value']}"
+              input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
+              run_measure(input_arguments, model)
+              runner = run_measure(input_arguments, model)
+              assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_max_value} for #{argument['name']}")
+              puts "Success: Testing argument #{argument['name']} max limit of #{argument['max_double_value']}"
+            end
+            #Check over max
+            if not argument['min_double_value'].nil?
+              puts "testing min limit"
+              model = OpenStudio::Model::Model.new
+              input_arguments = @good_input_arguments.clone
+              over_min_value = argument['min_double_value'].to_f - 1.0
+              over_min_value = over_max_value.to_s if argument['type'].downcase == "StringDouble".downcase
+              input_arguments[argument['name']] = over_min_value
+              puts "Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
+              input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
+              runner = run_measure(input_arguments, model)
+              assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_min_value} for #{argument['name']}")
+              puts "Success:Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
+            end
 
-    (@measure_interface_detailed).each_with_index do |argument|
-      if argument['type'] == 'Double' or argument['type'] == 'StringDouble'
-        puts "testing range for #{argument['name']} "
-        #Check over max
-        if not argument['max_double_value'].nil?
-          puts "testing max limit"
-          model = OpenStudio::Model::Model.new
-          input_arguments = @good_input_arguments.clone
-          over_max_value = argument['max_double_value'].to_f + 1.0
-          over_max_value = over_max_value.to_s if argument['type'].downcase == "StringDouble".downcase
-          input_arguments[argument['name']] = over_max_value
-          puts "Testing argument #{argument['name']} max limit of #{argument['max_double_value']}"
-          input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
-          run_measure(input_arguments, model)
-          runner = run_measure(input_arguments, model)
-          assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_max_value} for #{argument['name']}")
-          puts "Success: Testing argument #{argument['name']} max limit of #{argument['max_double_value']}"
+          end
+          if (argument['type'] == 'StringDouble') and (not argument["valid_strings"].nil?) and @use_string_double
+            model = OpenStudio::Model::Model.new
+            input_arguments = @good_input_arguments.clone
+            input_arguments[argument['name']] = SecureRandom.uuid.to_s
+            puts "Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
+            input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
+            runner = run_measure(input_arguments, model)
+            assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_min_value} for #{argument['name']}")
+          end
         end
-        #Check over max
-        if not argument['min_double_value'].nil?
-          puts "testing min limit"
-          model = OpenStudio::Model::Model.new
-          input_arguments = @good_input_arguments.clone
-          over_min_value = argument['min_double_value'].to_f - 1.0
-          over_min_value = over_max_value.to_s if argument['type'].downcase == "StringDouble".downcase
-          input_arguments[argument['name']] = over_min_value
-          puts "Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
-          input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
-          runner = run_measure(input_arguments, model)
-          assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_min_value} for #{argument['name']}")
-          puts "Success:Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
-        end
-
-      end
-      if (argument['type'] == 'StringDouble') and (not argument["valid_strings"].nil?) and @use_string_double
-        model = OpenStudio::Model::Model.new
-        input_arguments = @good_input_arguments.clone
-        input_arguments[argument['name']] = SecureRandom.uuid.to_s
-        puts "Testing argument #{argument['name']} min limit of #{argument['min_double_value']}"
-        input_arguments = {'json_input' => JSON.pretty_generate(input_arguments)} if @use_json_package
-        runner = run_measure(input_arguments, model)
-        assert(runner.result.value.valueName != 'Success', "Checks did not stop a lower than limit value of #{over_min_value} for #{argument['name']}")
       end
     end
-
   end
 
   # helper method to create necb archetype as a starting point for testing.
@@ -251,6 +265,9 @@ module BTAPMeasureTestHelper
 
     # This will create a instance of the measure you wish to test. It does this based on the test class name.
     measure = get_measure_object()
+    measure.use_json_package = @use_json_package
+    measure.use_string_double = @use_string_double
+    model = OpenStudio::Model::Model.new
     # Return false if can't
     return false if false == measure
     arguments = measure.arguments(model)
