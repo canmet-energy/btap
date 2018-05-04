9a1064e5b0571dfe087f4fc3213fc09bdec36553
phylroy <phylroy.lopez@gmail.com>
2018-05-04 18:40:23 +0000

    set method into measure proper.

--- measures/BTAPCreateNECBReferenceBuilding/tests/BTAPCreateNECBReferenceBuilding_test.rb
+++ measures/BTAPCreateNECBReferenceBuilding/tests/BTAPCreateNECBReferenceBuilding_test.rb
@@ -11,162 +11,6 @@ require_relative '../measure.rb'
 require_relative '../resources/BTAPMeasureHelper.rb'
 require 'minitest/autorun'
 
-class NECB2011
-
-  def model_modify_oa_controller(model)
-    #do nothing
-  end
-
-  def model_reset_or_room_vav_minimum_damper(model,model1)
-    #do nothing
-  end
-
-  def validate_initial_model(model)
-
-    if model.getBuildingStorys.empty?
-      OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Please assign Spaces to BuildingStorys in the geometry model.")
-    end
-    if model.getThermalZones.empty?
-      OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Please assign Spaces to ThermalZones in the geometry model.")
-    end
-    if model.getBuilding.standardsNumberOfStories.empty?
-      OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Please define Building.standardsNumberOfStories in the geometry model.")
-    end
-    if model.getBuilding.standardsNumberOfAboveGroundStories.empty?
-      OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Please define Building.standardsNumberOfAboveStories in the geometry model.")
-    end
-
-    if @space_type_map.nil? || @space_type_map.empty?
-      @space_type_map = get_space_type_maps_from_model(model)
-      if @space_type_map.nil? || @space_type_map.empty?
-        OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Please assign SpaceTypes in the geometry model or in standards database #{@space_type_map}.")
-      else
-        @space_type_map = @space_type_map.sort.to_h
-        OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "Loaded space type map from osm file")
-      end
-    end
-
-    # ensure that model is intersected correctly.
-    model.getSpaces.each { |space1| model.getSpaces.each { |space2| space1.intersectSurfaces(space2) } }
-    # Get multipliers from TZ in model. Need this for HVAC contruction.
-    @space_multiplier_map = {}
-    model.getSpaces.sort.each do |space|
-      @space_multiplier_map[space.name.get] = space.multiplier if space.multiplier > 1
-    end
-    OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', 'Finished adding geometry')
-    unless @space_multiplier_map.empty?
-      OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "Found mulitpliers for space #{@space_multiplier_map}")
-    end
-    return model
-  end
-
-  def model_apply_standard_to_model( model, epw_file, sizing_run_dir = Dir.pwd, debug = false, measure_model = nil)
-    climate_zone = 'NECB HDD Method'
-
-    self.validate_initial_model(model)
-
-    #Apply Climate and standard sizing and some housekeeping.
-    model.yearDescription.get.setDayofWeekforStartDay('Sunday')
-    model_add_design_days_and_weather_file(model, climate_zone, epw_file) # Standards
-    model_add_ground_temperatures(model, nil, climate_zone) # prototype candidate
-    model_apply_sizing_parameters(model)
-    model.getOutputControlReportingTolerances.setToleranceforTimeHeatingSetpointNotMet(1.0)
-    model.getOutputControlReportingTolerances.setToleranceforTimeCoolingSetpointNotMet(1.0)
-    model_temp_fix_ems_references(model)
-    model_modify_surface_convection_algorithm(model)
-
-
-    #Apply Default Loads
-    model.getThermostatSetpointDualSetpoints(&:remove)
-    model_add_loads(model) # standards candidate
-
-    #Apply Standards to Infiltraton
-    model_apply_infiltration_standard(model) # standards candidate
-    set_occ_sensor_spacetypes(model, @space_type_map)
-    model_add_daylighting_controls(model) # to be removed after refactor.
-
-
-    model_add_constructions(model, "NECB", climate_zone) # prototype candidate
-    apply_standard_construction_properties(model) # standards candidate
-    apply_standard_window_to_wall_ratio(model) # standards candidate
-    apply_standard_skylight_to_roof_ratio(model) # standards candidate
-
-
-    #Apply Default HVAC Systems.
-    model_create_thermal_zones(model, @space_multiplier_map) # standards candidate
-    raise("sizing run 0 failed!") if model_run_sizing_run(model, "#{sizing_run_dir}/SR0") == false
-    model_add_hvac(model, epw_file) # standards for NECB Prototype for NREL candidate
-    raise("sizing run 1 failed!") if model_run_sizing_run(model, "#{sizing_run_dir}/SR1") == false
-    model.getAirTerminalSingleDuctVAVReheats.each {|iobj| air_terminal_single_duct_vav_reheat_set_heating_cap(iobj)}
-    model_modify_oa_controller(model)
-    model_apply_prototype_hvac_assumptions(model, nil, climate_zone)
-    model_apply_hvac_efficiency_standard(model, climate_zone)
-
-    model_request_timeseries_outputs(model) if debug
-    # If measure model is passed, then replace measure model with new model created here.
-    if measure_model.nil?
-      return model
-    else
-      model_replace_model(measure_model, model)
-      return measure_model
-    end
-  end
-
-  def model_create_prototype_model(climate_zone, epw_file, sizing_run_dir = Dir.pwd, debug = false, measure_model = nil)
-    building_type = @instvarbuilding_type
-    raise 'no building_type!' if @instvarbuilding_type.nil?
-    model = nil
-    # prototype generation.
-    model = load_initial_osm(@geometry_file) # standard candidate
-
-    model.yearDescription.get.setDayofWeekforStartDay('Sunday')
-    model_add_design_days_and_weather_file(model, climate_zone, epw_file) # Standards
-    model_add_ground_temperatures(model, @instvarbuilding_type, climate_zone) # prototype candidate
-    model_apply_sizing_parameters(model)
-    model.getOutputControlReportingTolerances.setToleranceforTimeHeatingSetpointNotMet(1.0)
-    model.getOutputControlReportingTolerances.setToleranceforTimeCoolingSetpointNotMet(1.0)
-    model_temp_fix_ems_references(model)
-    model_modify_surface_convection_algorithm(model)
-
-    model.getThermostatSetpointDualSetpoints(&:remove)
-    model_add_loads(model) # standards candidate
-
-
-    model_apply_infiltration_standard(model) # standards candidate
-    set_occ_sensor_spacetypes(model, @space_type_map)
-    model_add_daylighting_controls(model)
-
-
-    model_add_constructions(model, 'NECB', climate_zone) # prototype candidate
-    apply_standard_construction_properties(model) # standards candidate
-    apply_standard_window_to_wall_ratio(model) # standards candidate
-    apply_standard_skylight_to_roof_ratio(model) # standards candidate
-
-
-    model_create_thermal_zones(model, @space_multiplier_map) # standards candidate
-    raise("sizing run 0 failed!") if model_run_sizing_run(model, "#{sizing_run_dir}/SR0") == false
-    model_add_hvac(model, epw_file) # standards for NECB Prototype for NREL candidate
-    raise("sizing run 1 failed!") if model_run_sizing_run(model, "#{sizing_run_dir}/SR1") == false
-    model.getAirTerminalSingleDuctVAVReheats.each {|iobj| air_terminal_single_duct_vav_reheat_set_heating_cap(iobj)}
-    model_apply_prototype_hvac_assumptions(model, building_type, climate_zone)
-    model_apply_hvac_efficiency_standard(model, climate_zone)
-
-    model_request_timeseries_outputs(model) if debug
-    # If measure model is passed, then replace measure model with new model created here.
-    if measure_model.nil?
-      return model
-    else
-      model_replace_model(measure_model, model)
-      return measure_model
-    end
-  end
-
-
-
-end
-
-
-
 class BTAPCreateNECBReferenceBuilding_Test < Minitest::Test
   # Brings in helper methods to simplify argument testing of json and standard argument methods.
   include(BTAPMeasureTestHelper)
@@ -235,20 +79,17 @@ class BTAPCreateNECBReferenceBuilding_Test < Minitest::Test
 
     )
 
-    necb = Standard.build("NECB2011")
-    model = BTAP::FileIO.load_osm(File.dirname(__FILE__) + '/../resources/NECB2011FullServiceRestaurant.osm')
-    measure_model = necb.model_apply_standard_to_model(model, 'CAN_BC_Vancouver.Intl.AP.718920_CWEC2016.epw')
+    #necb = Standard.build("NECB2011")
+    measure_model = BTAP::FileIO.load_osm(File.dirname(__FILE__) + '/../resources/NECB2011FullServiceRestaurant.osm')
 
-
-    #necb = Standard.build('NECB2011')
 
 
 
     # Create an instance of the measure
-    #runner = run_measure(input_arguments, model)
-    #puts model
+    runner = run_measure(input_arguments, measure_model)
+    puts show_output(runner.result)
     puts BTAP::FileIO.compare_osm_files(true_model, measure_model)
-    #puts show_output(runner.result)
+
 
     #assert(runner.result.value.valueName == 'Success')
   end
