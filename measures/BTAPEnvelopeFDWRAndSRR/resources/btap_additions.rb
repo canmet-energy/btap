9565deaa8b0145697e928f77265f3e6b0dfaedfb
phylroy <phylroy.lopez@gmail.com>
2018-05-29 19:47:42 +0000

    moved folders

--- measures/BTAPEnvelopeFDWRAndSRR/resources/btap_additions.rb
+++ measures/BTAPEnvelopeFDWRAndSRR/resources/btap_additions.rb
@@ -1,4 +1,4 @@
-require_relative('Standards.Construction')
+require_relative('Standards.Construction.Construction')
 require_relative('Standards.Model')
 require_relative('fileio')
 require_relative('building_envelope')
